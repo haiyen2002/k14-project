e5974527282f28b149a74f106f1e5b65645f2e25
ducnguyen995 <nguyenvanduc1043@gmail.com>
2021-10-21 17:58:27 +0700

    fix

--- public/assets/js/filter.js
+++ public/assets/js/filter.js
@@ -1,47 +1,50 @@
-
-
-$(document).ready(function() {
-    var type = "";
-    var priceMin = 0;
-    var priceMax = 100000000;
-    var products;
-    $.ajax({
-        url: `/filter/`,
-        method:'POST',
-        data: {type}
-    }).then(res => {
-        products = res.products;
-    }).catch(err => console.log(err))
-    $(document).find('.filter-checkbox').each(function() {
-        $(this).on('click', ()=> {
-                type = $(this).val()
-                $.ajax({
-                    url: `/filter/`,
-                    method:'POST',
-                    data: {type}
-                }).then(res => {
-                    products = res.products;
-                    render()
-                }).catch(err => console.log(err))
-                
-        })
-    });
-    $(document).find('.price-checkbox').each(function() {
-        $(this).on('click', ()=> {
-            priceMin = $(this).attr("min")
-            priceMax = $(this).attr("max")
-            render();
-        })
+$(document).ready(function () {
+  var type = "";
+  var priceMin = 0;
+  var priceMax = 100000000;
+  var products;
+  $.ajax({
+    url: `/filter/`,
+    method: "POST",
+    data: { type },
+  })
+    .then((res) => {
+      products = res.products;
     })
+    .catch((err) => console.log(err));
+    $(".filter-checkbox")
+    .each(function () {
+      $(this).on("click", () => {
+        type = $(this).val();
+        $.ajax({
+          url: `/filter/`,
+          method: "POST",
+          data: { type },
+        })
+          .then((res) => {
+            products = res.products;
+            render();
+          })
+          .catch((err) => console.log(err));
+      });
+    });
+    $(".price-checkbox")
+    .each(function () {
+      $(this).on("click", () => {
+        priceMin = $(this).attr("min");
+        priceMax = $(this).attr("max");
+        render();
+      });
+    });
 
-    function render(){
-        $('.filter-product-results').empty()
-                        for(var i = 0; i < products.length; i++){    
-                            var price = products[i].price.slice(0,products[i].price.length-1).replace(/,/g,'')
-                            if ( parseInt(price) <= priceMax && parseInt(price) >= priceMin){
-                                var productItem = `
+  function render() {
+    $(".filter-product-results").empty();
+    for (var i = 0; i < products.length; i++) {
+      var price = products[i].price.split(",").join("");
+      if (parseInt(price) <= priceMax && parseInt(price) >= priceMin) {
+        var productItem = `
                                 <div class="filter-col filter-col-3 currProduct" type="${products[i].prd_key}" price="${products[i].price}">   
-                                    <div class="product-filter__card">
+                                    <a class="product-filter__card" href="/product/detail/${products[i]._id}">
                                         <div class="product-filter-card__top">
                                             <img src="${products[i].img[0]}" alt="">
                                         </div>
@@ -49,12 +52,11 @@ $(document).ready(function() {
                                             <div class="product-filter-card__name">${products[i].name}</div>
                                             <div class="product-filter-card__price">${products[i].price}</div>
                                         </div>
-                                    </div>          
+                                    </a>          
                                 </div>
-                                `
-                                $('.filter-product-results').append(productItem)
-                            }                    
-                        }
+                                `;
+        $(".filter-product-results").append(productItem);
+      }
     }
-})
-
+  }
+});
