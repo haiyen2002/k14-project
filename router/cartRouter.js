2e91d300e0c8a6bf249eb0d023ed844baad6ab5f
ducnguyen995 <nguyenvanduc1043@gmail.com>
2021-11-08 15:42:33 +0700

    8/11

--- router/cartRouter.js
+++ router/cartRouter.js
@@ -3,13 +3,9 @@ const cartController = require("../controllers/cartController");
 const { route } = require("./UserRouter");
 const {orderssModel} = require("../models/db_mongoose")
 
-//get Cart Page
-router.get('/',cartController.getCart)
-
-
 // update Cart
 
-// router.put("/cart/", cartController.postCart);
+router.put("/cart/", cartController.postCart);
 
 //create order
 
@@ -21,8 +17,5 @@ router.get("/check", cartController.getUpCart);
 //
 
 router.delete("/cancel/:id", cartController.cancelOrder ) 
-
-//add cart to database
-router.put('/add', cartController.addCart) 
 
 module.exports = router;
